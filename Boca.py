d4ced3d8203341e9e4321d67b33c5c917a67a05b
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-09-08 09:07:25 +0300

    Create Boca.py

---
+++ Boca.py
@@ -0,0 +1,31 @@
+import time
+
+
+t1 = time.time()
+x = "It's a nice day a bit windy" 
+x1 = "There will be many changes" 
+x2 = "There is a Google, Gogle, Gogol thing and Dead souls" 
+x3 = "Repetance" 
+x4 = "a talk about toothbrushes and emotions"
+x5 = "or a talk about toothbrushes and emojicode"
+x6 = "SOAD"
+x7 = "No boca, but Stevia?"
+x8 = "Something from Nothing"
+x9 = "Have a nice day!"
+
+
+print("but that's cool")
+print(bool(x1))
+print("Tribe")
+print(bool(x3))
+print("¿!(boc)")
+print(bool(x5))
+print("Wake up Grab a brush and put a little make-up")
+print("for health issues..")
+print("A short history of the failure of mankind")
+print(bool(x9))
+
+
+t2 = time.time()
+t = t2 - t1
+print("Elapsed time is : ", t, " seconds")
