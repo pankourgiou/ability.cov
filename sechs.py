a1637fa985db63191fb535334e75df4365e32689
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-05-15 12:51:46 +0300

    Update and rename Я31.pas to sechs.py

---
+++ sechs.py
@@ -0,0 +1,12 @@
+import time
+import antigravity
+
+start = time.time()
+print("六+---=六")
+
+xyz = "六+---=六"
+
+print(bool(xyz))
+
+end = time.time()
+print(end - start)
--- Я31.pas
+++
@@ -1,8 +0,0 @@
-#from urllib.request import urlopen
-#page=urlopen("http://freesupertips.com/")
-
-#Fetches the code
-#of the webpage
-#content = page.read()
-
-#print(content)
